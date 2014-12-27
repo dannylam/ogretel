0aace2786bf2bc5be3e5f67fc85300657b7a0329
Danny Lam <dannylam@live.se>
2014-12-27 16:50:43 +0100

    Done with TODO: Javarock IBPFH

--- Papyrus/Make a Booking/src/bookingmodel/IBookingProvidesForHost.java
+++ Papyrus/Make a Booking/src/bookingmodel/IBookingProvidesForHost.java
@@ -5,59 +5,55 @@ package bookingmodel;
 import org.eclipse.emf.ecore.EObject;
 
 /**
- * TODO: Javadoc, glöm inte markera generated NOT när man är klar
- * <!-- begin-user-doc -->
- * A representation of the maintenancemodel object '<em><b>IBooking Provides For Host</b></em>'.
- * <!-- end-user-doc -->
- *
- *
+ * This class is a representation of an interface of IBookingProvidesForHost, extedning EObject.
+ * It has 5 methods;
+ * enableSelfManagement - enable self management.
+ * addServiceNote - add a specific service note.
+ * editServiceNote - edit a specific service note
+ * removeServiceNote - remove a specific service note.
+ * getServiceNote - get a specific service note.
  * @see bookingmodel.ModelPackage#getIBookingProvidesForHost()
  * @maintenancemodel interface="true" abstract="true"
- * @generated
+ * @generated NOT
  */
 public interface IBookingProvidesForHost extends EObject {
 	/**
-	 * TODO: Javadoc, glöm inte markera generated NOT när man är klar
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * Enable self management to a specific host.
+	 * @param guestEmail
 	 * @maintenancemodel guestEmailDataType="org.eclipse.uml2.types.String" guestEmailRequired="true" guestEmailOrdered="false"
-	 * @generated
+	 * @generated NOT
 	 */
 	void enableSelfManagement(String guestEmail);
 
 	/**
-	 * TODO: Javadoc, glöm inte markera generated NOT när man är klar
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * Add a specific service note.
+	 * @param serviceNote
 	 * @maintenancemodel serviceNoteDataType="org.eclipse.uml2.types.String" serviceNoteRequired="true" serviceNoteOrdered="false"
-	 * @generated
+	 * @generated NOT
 	 */
 	void addServiceNote(String serviceNote);
 
 	/**
-	 * TODO: Javadoc, glöm inte markera generated NOT när man är klar
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * Edit a service note by changing a specific service note into a new service note.
+	 * @param oldServiceNote, newServiceNote
 	 * @maintenancemodel oldServiceNoteDataType="org.eclipse.uml2.types.String" oldServiceNoteRequired="true" oldServiceNoteOrdered="false" newServiceNoteDataType="org.eclipse.uml2.types.String" newServiceNoteRequired="true" newServiceNoteOrdered="false"
-	 * @generated
+	 * @generated NOT
 	 */
 	void editServiceNote(String oldServiceNote, String newServiceNote);
 
 	/**
-	 * TODO: Javadoc, glöm inte markera generated NOT när man är klar
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * Remove a specific service note
+	 * @param serviceNote
 	 * @maintenancemodel serviceNoteDataType="org.eclipse.uml2.types.String" serviceNoteRequired="true" serviceNoteOrdered="false"
-	 * @generated
+	 * @generated NOT 
 	 */
 	void removeServiceNote(String serviceNote);
 
 	/**
-	 * TODO: Javadoc, glöm inte markera generated NOT när man är klar
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * Get service notes from a specific booking.
+	 * @param bookingRef
 	 * @maintenancemodel bookingRefDataType="org.eclipse.uml2.types.String" bookingRefRequired="true" bookingRefOrdered="false"
-	 * @generated
+	 * @generated NOT
 	 */
 	void getServiceNotes(String bookingRef);
 
