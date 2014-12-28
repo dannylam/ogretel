de5f0bad5ed1ca791f65b84824373e3770bc9dfb
Danny Lam <dannylam@live.se>
2014-12-28 10:39:10 +0100

    Done with TODO: Javadoc PaymentDetails

--- Papyrus/Make a Booking/src/bookingmodel/PaymentDetails.java
+++ Papyrus/Make a Booking/src/bookingmodel/PaymentDetails.java
@@ -5,10 +5,16 @@ package bookingmodel;
 import org.eclipse.emf.ecore.EObject;
 
 /**
- * TODO: Javadoc, glöm inte markera generated NOT när man är klar
- * <!-- begin-user-doc -->
- * A representation of the maintenancemodel object '<em><b>Payment Details</b></em>'.
- * <!-- end-user-doc -->
+ * A representation of the interface PaymentDetails, extending Eobject.
+ * This class has 8 methods;
+ * getCcNr - returns the credit card number.
+ * setCcNr - sets the credit card number.
+ * getCcV - returns the card code verification.
+ * setCcV - sets the card code verification.
+ * getExpMonth - returns the expiration month.
+ * setExpMonth - sets the expiration month.
+ * getExpYear - returns the expiration year.
+ * setExpYear - sets the expiration year.
  *
  * <p>
  * The following features are supported:
@@ -26,14 +32,8 @@ import org.eclipse.emf.ecore.EObject;
  */
 public interface PaymentDetails extends EObject {
 	/**
-	 * Returns the value of the '<em><b>Cc Nr</b></em>' attribute.
-	 * TODO: Javadoc, glöm inte markera generated NOT när man är klar
-	 * <!-- begin-user-doc -->
-	 * <p>
-	 * If the meaning of the '<em>Cc Nr</em>' attribute isn't clear,
-	 * there really should be more of a description here...
-	 * </p>
-	 * <!-- end-user-doc -->
+	 * Returns the value of the '<em><b>Cc Nr</b></em>' attribute,
+	 * that is the credit card number.
 	 * @return the value of the '<em>Cc Nr</em>' attribute.
 	 * @see #setCcNr(String)
 	 * @see bookingmodel.ModelPackage#getPaymentDetails_CcNr()
@@ -43,10 +43,8 @@ public interface PaymentDetails extends EObject {
 	String getCcNr();
 
 	/**
-	 * Sets the value of the '{@link bookingmodel.PaymentDetails#getCcNr <em>Cc Nr</em>}' attribute.
-	 * TODO: Javadoc, glöm inte markera generated NOT när man är klar
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * Sets the value of the '{@link bookingmodel.PaymentDetails#getCcNr <em>Cc Nr</em>}' attribute,
+	 * that is the credit card number.
 	 * @param value the new value of the '<em>Cc Nr</em>' attribute.
 	 * @see #getCcNr()
 	 * @generated
@@ -54,14 +52,8 @@ public interface PaymentDetails extends EObject {
 	void setCcNr(String value);
 
 	/**
-	 * Returns the value of the '<em><b>Cc V</b></em>' attribute.
-	 * TODO: Javadoc, glöm inte markera generated NOT när man är klar
-	 * <!-- begin-user-doc -->
-	 * <p>
-	 * If the meaning of the '<em>Cc V</em>' attribute isn't clear,
-	 * there really should be more of a description here...
-	 * </p>
-	 * <!-- end-user-doc -->
+	 * Returns the value of the '<em><b>Cc V</b></em>' attribute,
+	 * that is the card code verification.
 	 * @return the value of the '<em>Cc V</em>' attribute.
 	 * @see #setCcV(String)
 	 * @see bookingmodel.ModelPackage#getPaymentDetails_CcV()
@@ -71,10 +63,8 @@ public interface PaymentDetails extends EObject {
 	String getCcV();
 
 	/**
-	 * Sets the value of the '{@link bookingmodel.PaymentDetails#getCcV <em>Cc V</em>}' attribute.
-	 * TODO: Javadoc, glöm inte markera generated NOT när man är klar
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * Sets the value of the '{@link bookingmodel.PaymentDetails#getCcV <em>Cc V</em>}' attribute,
+	 * that is the card code verification.
 	 * @param value the new value of the '<em>Cc V</em>' attribute.
 	 * @see #getCcV()
 	 * @generated
@@ -82,14 +72,8 @@ public interface PaymentDetails extends EObject {
 	void setCcV(String value);
 
 	/**
-	 * Returns the value of the '<em><b>Exp Month</b></em>' attribute.
-	 * TODO: Javadoc, glöm inte markera generated NOT när man är klar
-	 * <!-- begin-user-doc -->
-	 * <p>
-	 * If the meaning of the '<em>Exp Month</em>' attribute isn't clear,
-	 * there really should be more of a description here...
-	 * </p>
-	 * <!-- end-user-doc -->
+	 * Returns the value of the '<em><b>Exp Month</b></em>' attribute,
+	 * that is the expiration month.
 	 * @return the value of the '<em>Exp Month</em>' attribute.
 	 * @see #setExpMonth(int)
 	 * @see bookingmodel.ModelPackage#getPaymentDetails_ExpMonth()
@@ -99,10 +83,8 @@ public interface PaymentDetails extends EObject {
 	int getExpMonth();
 
 	/**
-	 * Sets the value of the '{@link bookingmodel.PaymentDetails#getExpMonth <em>Exp Month</em>}' attribute.
-	 * TODO: Javadoc, glöm inte markera generated NOT när man är klar
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * Sets the value of the '{@link bookingmodel.PaymentDetails#getExpMonth <em>Exp Month</em>}' attribute,
+	 * that is the expiration month.
 	 * @param value the new value of the '<em>Exp Month</em>' attribute.
 	 * @see #getExpMonth()
 	 * @generated
@@ -110,14 +92,8 @@ public interface PaymentDetails extends EObject {
 	void setExpMonth(int value);
 
 	/**
-	 * Returns the value of the '<em><b>Exp Year</b></em>' attribute.
-	 * TODO: Javadoc, glöm inte markera generated NOT när man är klar
-	 * <!-- begin-user-doc -->
-	 * <p>
-	 * If the meaning of the '<em>Exp Year</em>' attribute isn't clear,
-	 * there really should be more of a description here...
-	 * </p>
-	 * <!-- end-user-doc -->
+	 * Returns the value of the '<em><b>Exp Year</b></em>' attribute, 
+	 * that is the expiration year.
 	 * @return the value of the '<em>Exp Year</em>' attribute.
 	 * @see #setExpYear(int)
 	 * @see bookingmodel.ModelPackage#getPaymentDetails_ExpYear()
@@ -127,10 +103,8 @@ public interface PaymentDetails extends EObject {
 	int getExpYear();
 
 	/**
-	 * Sets the value of the '{@link bookingmodel.PaymentDetails#getExpYear <em>Exp Year</em>}' attribute.
-	 * TODO: Javadoc, glöm inte markera generated NOT när man är klar
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * Sets the value of the '{@link bookingmodel.PaymentDetails#getExpYear <em>Exp Year</em>}' attribute,
+	 * that is the expiration year.
 	 * @param value the new value of the '<em>Exp Year</em>' attribute.
 	 * @see #getExpYear()
 	 * @generated
