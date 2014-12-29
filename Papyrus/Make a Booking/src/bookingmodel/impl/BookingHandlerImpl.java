3296a40a5978717f195bfffc47876c2c46e8b7c8
Paula Eriksson <paula.eriksson@gmail.com>
2014-12-29 15:09:29 +0100

    fixfix what

--- Papyrus/Make a Booking/src/bookingmodel/impl/BookingHandlerImpl.java
+++ Papyrus/Make a Booking/src/bookingmodel/impl/BookingHandlerImpl.java
@@ -121,38 +121,20 @@ public class BookingHandlerImpl extends MinimalEObjectImpl.Container implements 
 
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * A method which edits a booking such as the start date, end date, 
+	 * number of guests, room type and extras.
+	 * @param bookingRef, startDate, endDate, nrOfGuests, roomTypes, extras
 	 * @generated NOT
 	 */
 	public int editBooking(String bookingRef, String startDate, String endDate, int nrOfGuests, List<String> roomTypes, List<String> extras) {
-		// TODO: implement this method
-		// Ensure that you remove @generated or mark it @generated NOT
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated NOT
-	 */
-	public int editBooking(String bookingRef, String startDate, String endDate, int nrOfGuests, String roomTypes, String extras) {
 		int result = 0;
-		// TODO: implement this method
-		/*check which inparams which are null, these are not to be changed, and have their oldvalue
-		 * then create a new booking with the old booking reference */
-		 
-			if(this.exists(bookingRef)) {			//kolla om booking ref finns
-					
-					this.getBooking(bookingRef).setStartDate(startDate);
-					
-					this.getBooking(bookingRef).setEndDate(endDate);
-					
-					this.getBooking(bookingRef).setNrOfGuests(nrOfGuests);
-	
-			//		this.getBooking(bookingRef).setRoomTypes(roomTypes);
-					
-			//		this.getBooking(bookingRef).setExtras(extras);
+		
+			if(this.exists(bookingRef)){
+				this.getBooking(bookingRef).setStartDate(startDate);
+				this.getBooking(bookingRef).setEndDate(endDate);
+				this.getBooking(bookingRef).setNrOfGuests(nrOfGuests);
+				this.getBooking(bookingRef).setRoomTypes(roomTypes);
+				this.getBooking(bookingRef).setExtras(extras);
 			}
 		return result;
 	}
