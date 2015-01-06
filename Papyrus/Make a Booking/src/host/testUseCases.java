3282c6b752ceed3dd09f1730b1b4b5263c0a3258
Patricia <chorriballong@gmail.com>
2015-01-06 15:31:29 +0100

    stubs for checkin and check out alternative test

--- Papyrus/Make a Booking/src/host/testUseCases.java
+++ Papyrus/Make a Booking/src/host/testUseCases.java
@@ -117,6 +117,66 @@ public class testUseCases {
 		assertTrue(checkin == -1);
 	}
 	
+	/*
+	 * Alternative flow CheckOut: Room ID does not exist
+	 */
+	@Test
+	public void testCheckOutAltFlow2a(){
+
+	}
+	
+	/*
+	 * Alternative flow CheckOut: Room ID status is not busy
+	 */
+	@Test
+	public void testCheckOutAltFlow2b(){
+
+	}
+	
+	/*
+	 * Alternative flow CheckOut: The guest ID does not indicate a guest
+	 * responsible for a room in the booking.
+	 */
+	@Test
+	public void testCheckOutAltFlow2c(){
+
+	}
+	
+	/*
+	 * Alternative flow CheckOut: Another guest than indicated by the guest
+	 *  ID pays for the extras associated to the room for which they are responsible.
+	 */
+	@Test
+	public void testCheckOutAltFlow4a(){
+
+	}
+	
+	/*
+	 * Alternative flow CheckOut: Payment details are invalid.
+	 */
+	@Test
+	public void testCheckOutAltFlow4b(){
+
+	}
+	
+	/*
+	 * Alternative flow CheckOut: The guest indicated by the guest ID is the last 
+	 * (responsible) guest and there is nothing left to pay.
+	 */
+	@Test
+	public void testCheckOutAltFlow4c(){
+
+	}
+	
+	/*
+	 * Alternative flow CheckOut: The guest indicated by the guest ID is the last (responsible) guest 
+	 * and there is something left to pay.
+	 */
+	@Test
+	public void testCheckOutAltFlow4d(){
+
+	}
+	
 
 	@Test
 	public void testEditAvalabilityOfRoom() {
